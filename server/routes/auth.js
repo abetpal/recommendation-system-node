1915240c032286c82e73d14eaf2334ea4733d79a
abetpal <patelabhinav61@gmail.com>
2022-06-28 23:52:51 +0530

    Added comment

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,42 +1,69 @@
-const express= require('express')
-const router = express.Router()
-const mongoose=require('mongoose')
-const User=mongoose.model("User")
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
-router.post('/signup',(req,res)=>{
-    const{first_name,last_name,email,password,username,address,city,state,pincode,contact_number}=req.body
-    console.log(req.body)
-    if(!first_name || !last_name || !email || !password || !username || !address || !city || !state || !pincode || !contact_number){
-        return res.status(422).json({error :"please add all the fields"})
-    }
-    User.findOne({email:email})
-    .then((savedUser)=>{
-        if(savedUser){
-            return res.status(422).json({error:"user already exists with that email"})
-        }
-        const user=new User({
-            first_name,
-            last_name,
-            email,
-            password,
-            username,
-            address,
-            city,
-            state,
-            pincode,
-            contact_number
-        })
-        user.save()
-        .then(user=>{
-            res.json({message:"saved successfully"})
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-})
+// Todo: Complete Signup
 
-module.exports=router
+router.post("/signup", (req, res) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    username,
+    address,
+    city,
+    state,
+    pincode,
+    contact_number,
+  } = req.body;
+  console.log(req.body);
+  if (
+    !first_name ||
+    !last_name ||
+    !email ||
+    !password ||
+    !username ||
+    !address ||
+    !city ||
+    !state ||
+    !pincode ||
+    !contact_number
+  ) {
+    return res.status(422).json({ error: "please add all the fields" });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res
+          .status(422)
+          .json({ error: "user already exists with that email" });
+      }
+      const user = new User({
+        first_name,
+        last_name,
+        email,
+        password,
+        username,
+        address,
+        city,
+        state,
+        pincode,
+        contact_number,
+      });
+      user
+        .save()
+        .then((user) => {
+          res.json({ message: "saved successfully" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
